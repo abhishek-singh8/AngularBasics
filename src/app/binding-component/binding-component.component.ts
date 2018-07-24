19fe6de7dd74c688d10cac5280f6ad137a9ff7f6
abhishek <abhisheksingh4312@gmail.com>
2018-07-24 15:01:37 +0530

    property Binding and Class binding

--- src/app/binding-component/binding-component.component.ts
+++ src/app/binding-component/binding-component.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   template: `<div>   
              Welcome {{2+2 +name  }} to inline HTML 
              <h2>{{name.length}}</h2>
-             <h2>{{name.toUpperCase()}}</h2>
-             <h2>Method testing {{methodTest()}}</h2>
-             </div>`,
-             
-  styleUrls: ['./binding-component.component.css']
+             <h2 class="text-success">{{name.toUpperCase()}}</h2>
+             <h2 [class] ="specialText">Method testing {{methodTest()}}</h2>
+             <h2 [class.text-danger]="isTrue"> class.text-danger</h2>
+             <h2 [ngClass]="messageClasses"> NGCLASS  multiple class in one</h2>
+
+             </div>
+
+             <input type="text" value="Abhishek">
+             <input [id]="myId" type="text" value="summit">
+             <input id="{{myId}}" type="text" value="summit">
+             <input [disabled]="isDisabled" id="{{myId}}" type="text" value="summit">`;
+
+  styles: [`
+          .text-success{
+            color:green;
+          }
+          .text-danger{
+            color:red;
+          }
+          .text-special{
+            font-size:italic;
+          }
+         `]
 })
 
 
@@ -17,6 +35,16 @@ import { Component, OnInit } from '@angular/core';
 export class BindingComponentComponent implements OnInit {
   
   public name ="AbhishekSingh";
+  public myId ="testId";
+  public isDisabled="true";
+  public specialText="text-special"
+  public isTrue="true";
+  public isSpecial="true";
+  public messageClasses={
+         "text-success": this.isTrue;
+         "text-danger" : !this.isTrue;
+         "text-special": this.isSpecial;
+         }
   constructor() { }
   
   methodTest(){
